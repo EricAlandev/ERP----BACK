1626eae647165a4f87b4f1f4f7d682e44af82569
Seu Nome <seu@exemplo.com>
2026-07-24 17:34:44 -0300

    0.03.1 - Continuation of the contract pdf

--- src/main/java/boletoGenreator/infrastructure/controller/mapper/contracts/ContractPdfUseCase.java
+++ src/main/java/boletoGenreator/infrastructure/controller/mapper/contracts/ContractPdfUseCase.java
@@ -1,72 +1,63 @@
 package boletoGenreator.infrastructure.controller.mapper.contracts;
 
 import java.math.BigDecimal;
+import java.net.MalformedURLException;
 import java.time.LocalDateTime;
 
+import com.itextpdf.layout.Document;
 import com.itextpdf.layout.element.Cell;
+import com.itextpdf.layout.element.Image;
 import com.itextpdf.layout.element.Paragraph;
 import com.itextpdf.layout.element.Table;
 import com.itextpdf.layout.properties.TextAlignment;
+import com.itextpdf.layout.properties.UnitValue;
 
 import boletoGenreator.domain.model.contracts.DealContract;
 import boletoGenreator.infrastructure.controller.dto.generic.ParseTime;
 import boletoGenreator.useCases.UseCase;
 import boletoGenreator.useCases.service.pdfs.ItextFunctions;
 import boletoGenreator.useCases.service.pdfs.ItextFunctions.ManagerItext;
+import jakarta.transaction.Transactional;
 import lombok.Value;
 
 public class ContractPdfUseCase implements UseCase<ContractPdfUseCase.InputValues, ContractPdfUseCase.OutPutValues> {
-    
 
+    @Transactional
     @Override
     public OutPutValues execute(InputValues input){
 
         ManagerItext manageItext = ItextFunctions.PrepareteItext();
 
         Paragraph Tittle = new Paragraph("Contract Paper").setTextAlignment(TextAlignment.CENTER);
-
         manageItext.getDocument().add(Tittle);
 
         DealContract pdfData = input.getPdfData();
 
+        LocalDateTime today = LocalDateTime.now();
+
         //header
-        float [] pdfHeader = {33.f, 33.3f, 33.3f};
-
-        Table headerContract = new Table(pdfHeader);
-
-        headerContract.addCell(new Cell().add(new Paragraph("Client Name : " + pdfData.getNameClient())));
-
-        headerContract.addCell(new Cell().add(new Paragraph("Type Contract : " + pdfData.getBankBilletType())));
-
-        headerContract.addCell(new Cell().add(new Paragraph("Date Contract : " + LocalDateTime.now())));
-
-        manageItext.getDocument().add(headerContract);
+        createHeader(pdfData.getNameClient(), pdfData.getBankBilletType(), today, manageItext.getDocument());
 
         BigDecimal priceLoan = BigDecimal.ZERO;
 
         //Generate the lines of the installments
         for(int i = 0; i < pdfData.getQuantityInstallments(); i++){
-            
-            BigDecimal priceInstallment = pdfData.getPriceInstallments();
-
-            LocalDateTime today = LocalDateTime.now();
-            LocalDateTime dateInstallmente = today.plusDays(30 * i);
-            String formatedDate = ParseTime.parseTime(dateInstallmente);
-
-            float [] installmentDimensions = {50f, 50f};
-            Table intallmentTable = new Table(installmentDimensions);
-
-            intallmentTable.addCell(new Cell().add(new Paragraph("Value Installmente : " + priceInstallment)));
-
-            intallmentTable.addCell(new Cell().add(new Paragraph("Date : " + formatedDate)));
-
-            manageItext.getDocument().add(intallmentTable);
-            priceLoan.add(priceInstallment);
+            generateInstallmentLine(pdfData.getPriceInstallments(), today, manageItext.getDocument(), priceLoan, i);
         }
+
         Paragraph textInstallments  = new Paragraph("The client gonna pay " + pdfData.getQuantityInstallments() + " installments, and the loan gonna cost on total " + priceLoan).setTextAlignment(TextAlignment.RIGHT);
 
         manageItext.getDocument().add(textInstallments);
 
+        //Signatures
+        try {
+            Table signaturesTable = generateSignatures();
+            manageItext.getDocument().add(signaturesTable);
+
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("");
+        }
+        
         manageItext.getDocument().close();
 
         byte[] pdfBytes = manageItext.getOutput().toByteArray();
@@ -82,5 +73,66 @@ public class ContractPdfUseCase implements UseCase<ContractPdfUseCase.InputValue
     @Value
     public static class OutPutValues implements UseCase.OutPutValues{
         byte[] pdf;
+    }
+
+    public Table generateSignatures() throws MalformedURLException{
+            UnitValue[] signaturesProportion = {
+                UnitValue.createPercentValue(50),
+                UnitValue.createPercentValue(50)
+            };
+
+            Table signatureTables = new Table(signaturesProportion);
+
+            signatureTables.setWidth(UnitValue.createPercentValue(100));
+
+            Image managerSignature = ItextFunctions.createImage("/contract/BankManagerSignature.png");
+
+            Image clientSignature = ItextFunctions.createImage("/contract/ClientField.png");
+
+            signatureTables.addCell(managerSignature);
+            signatureTables.addCell(clientSignature);
+
+            return signatureTables;
+    }
+
+
+    public void generateInstallmentLine(BigDecimal priceInstallment, LocalDateTime today, Document document, BigDecimal priceLoan, int i){
+
+        LocalDateTime dateInstallmente = today.plusDays(30 * i);
+        String formatedDate = ParseTime.parseTime(dateInstallmente);
+
+        UnitValue[] installmentDimensions = {
+            UnitValue.createPercentValue(50),
+            UnitValue.createPercentValue(50)
+        };
+        Table intallmentTable = new Table(installmentDimensions);
+
+        intallmentTable.setWidth(UnitValue.createPercentValue(100));
+
+        intallmentTable.addCell(new Cell().add(new Paragraph("Value Installmente : " + priceInstallment)));
+
+        intallmentTable.addCell(new Cell().add(new Paragraph("Date : " + formatedDate)));
+
+        document.add(intallmentTable);
+        priceLoan = priceLoan.add(priceInstallment);
+    }
+
+    public void createHeader(String nameClient, String BankBilletType, LocalDateTime today, Document document){
+         UnitValue[] pdfHeader = {
+            UnitValue.createPercentValue(50),
+            UnitValue.createPercentValue(50)
+        };
+
+        Table headerContract = new Table(pdfHeader);
+
+        headerContract.setWidth(UnitValue.createPercentValue(100));
+
+        headerContract.addCell(new Cell().add(new Paragraph("Client Name : " + nameClient)));
+
+        headerContract.addCell(new Cell().add(new Paragraph("Type Contract : " + BankBilletType)));
+
+        headerContract.addCell(new Cell().add(new Paragraph("Date Contract : " +  today)));
+
+        document.add(headerContract);
     }
 }
--- src/main/java/boletoGenreator/useCases/service/pdfs/ItextFunctions.java
+++ src/main/java/boletoGenreator/useCases/service/pdfs/ItextFunctions.java
@@ -1,10 +1,14 @@
 package boletoGenreator.useCases.service.pdfs;
 
 import java.io.ByteArrayOutputStream;
+import java.net.URL;
 
+import com.itextpdf.io.image.ImageData;
+import com.itextpdf.io.image.ImageDataFactory;
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfWriter;
 import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Image;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -13,6 +17,16 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 public class ItextFunctions {
+
+    @Getter
+    @Setter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ManagerItext{
+        ByteArrayOutputStream output;
+        Document document;
+    }
     
     public static ManagerItext PrepareteItext(){
         ByteArrayOutputStream output = new 
@@ -33,13 +47,18 @@ public class ItextFunctions {
         return manager;
     }
 
-    @Getter
-    @Setter
-    @Builder
-    @NoArgsConstructor
-    @AllArgsConstructor
-    public static class ManagerItext{
-        ByteArrayOutputStream output;
-        Document document;
+    //find Path + generate signature Tables
+    public static Image createImage(String path){
+        URL url = ItextFunctions.findPath(path);
+
+        ImageData imageData = ImageDataFactory.create(url);
+        Image image = new Image(imageData);
+
+        return image;
+    }
+
+    private static URL findPath(String path){
+        URL url = ManagerItext.class.getClassLoader().getResource(path);
+        return url; 
     }
 }
